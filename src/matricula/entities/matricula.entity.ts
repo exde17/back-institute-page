b3a7bd983d1f60ae2ece0c7cfef52274f72c5289
Mario Begambre <mabg0630@gmail.com>
2025-12-09 10:54:48 -0500

    relations matricula inscripcion

--- src/matricula/entities/matricula.entity.ts
+++ src/matricula/entities/matricula.entity.ts
@@ -1,3 +1,4 @@
+import { Inscripcion } from "src/inscripcion/entities/inscripcion.entity";
 import { Pago } from "src/pago/entities/pago.entity";
 import { User } from "src/user/entities/user.entity";
 import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
@@ -55,4 +56,7 @@ export class Matricula {
 
   @OneToMany(() => Pago, (pago) => pago.matricula)
     pagos: Pago[];
+
+  @ManyToOne(()=> Inscripcion, (inscripcion) => inscripcion.matriculas)
+    inscripcion: Inscripcion;
 }
