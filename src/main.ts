0c89e82822e39879046f11f9e972b7c51cbd29ea
ggaleanoguerra <ggaleanoguerra@correo.unicordoba.edu.co>
2026-02-01 14:16:40 -0500

    feat: update CORS configuration to allow all origins

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,7 @@ async function bootstrap() {
   app.use(require('express').urlencoded({ limit: '50mb', extended: true }));
   
   app.enableCors({
-    origin: [
-      'http://localhost:4321',
-      'http://localhost:4320',
-      'https://institute.bg3sas.com',
-      'https://www.institute.bg3sas.com',
-    ],
+    origin: '*',
     credentials: true,
   });
   
